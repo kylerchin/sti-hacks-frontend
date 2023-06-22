ef295d097ee25ad77a34d7e69ec9e74bb3dc86ba
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-06-22 16:58:59 -0700

    firebase creds

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,3 +1,28 @@
+<script lang='ts'>
+   
+   // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKUJIq6bjpUFSn9krJ9D2cQupN8biIyfw",
+  authDomain: "sti-hackathon.firebaseapp.com",
+  projectId: "sti-hackathon",
+  storageBucket: "sti-hackathon.appspot.com",
+  messagingSenderId: "538088055127",
+  appId: "1:538088055127:web:117ad676c60ab848ee9e12",
+  measurementId: "G-VL922YFEJ1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+   </script>
+
 <h1>
     
     <h1 />
