accf8e359278819e82a2db7c55fda3d16493b8bc
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-06-22 16:00:50 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<h1>Duty to Warn</h1>
+
+<style>
+    h1 {
+        font-family: "IBM Plex Sans"
+    }
+    </style>
---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,9 @@
+<h1>
+    
+    <h1 />
+
+<style>
+    h1 {
+        font-family: "IBM Plex Sans"
+    }
+    </style>
