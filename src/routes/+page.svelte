abbf56252fe97e8409097618bf9462966f202143
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-06-23 09:30:02 -0700

    fix the front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,35 +3,34 @@
 	import Footer from './footer.svelte';
 </script>
 
-<div class="min-h-screen bruzbackground flex flex-col">
+<div class="min-h-screen bruzbackground bg-[#1f3942] flex flex-col">
 	<Header />
-	<div class="mx-4 mt-36" id="text-body">
-		<h1 class="text-gray-50 px-2 py-1 max-w-md text-left">Rheia</h1>
+	<div class="mx-4 mt-36 w-full" id="text-body">
+		<div class="md:grid md:grid-cols-2 md:gap-x-2 w-full">
+			<div class="hidden md:block">PUT THE GOD DAMN IMAGE HERE</div>
+			<div class="">
+				<h1 class="text-gray-50 px-2 py-1 max-w-md text-left">Rheia</h1>
 
-		<h2 class="text-gray-50 px-2 py-3 max-w-md text-left">
-			Rheia is a contact-tracing risk-assessment software for physicians and DIS to facilitate STI
-			early prevention.
-		</h2>
+				<h2 class="text-gray-50 px-2 py-3 max-w-md text-left">
+					Rheia is a contact-tracing risk-assessment software for physicians and DIS to facilitate
+					STI early prevention.
+				</h2>
 
-		<div class="flex flex-row gap-x-7 mt-1 ml-2 py-0.5">
-			<a href="/signin?who=dis"><div class="px-3 py-2 bg-white text-black">Sign-in as DIS</div></a>
-			<a href="/signin?who=provider"
-				><div class="px-3 py-2 bg-[#446e70] text-white">Sign-in as provider</div></a
-			>
+				<div class="flex flex-row gap-x-3 mt-1 ml-2 py-0.5">
+					<a href="/signin?who=dis"
+						><div class="px-3 py-2 bg-white text-black">Sign-in as DIS</div></a
+					>
+					<a href="/signin?who=provider"
+						><div class="px-3 py-2 bg-[#446e70] text-white">Sign-in as provider</div></a
+					>
+				</div>
+			</div>
 		</div>
 	</div>
 	<Footer />
 </div>
 
 <style>
-	#text-body {
-		padding-left: 1000px;
-		padding-top: 70px;
-		align-items: left;
-		display: flex;
-		flex-direction: column;
-	}
-
 	h1 {
 		font-family: 'IBM Plex Sans';
 		font-size: 50px;
@@ -42,7 +41,6 @@
 	}
 
 	.bruzbackground {
-		background-color: #1f3942;
 		/*background: linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url("/dennis-mita-qmg0MidYviw-unsplash.jpg") no-repeat;*/
 		background-size: 100%;
 	}
