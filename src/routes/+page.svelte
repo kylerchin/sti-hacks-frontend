a3a23b67d60bfcd72ecfe2308481e8d5fede293f
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-06-23 00:21:02 -0700

    send to signin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,10 @@
 <div class="min-h-screen bruzbackground flex flex-col">
 <div class="mx-4 mt-36">
 <h1 class="font-bold text-gray-50 px-2 py-1 max-w-md">Rheia is a contact-tracing risk-assessment software for doctors and DIS to facilitate STI early prevention.</h1>
-<br/>
-<div class="flex flex-row"><div>Sign-in as DIS</div></div>
+
+<div class="flex flex-row gap-x-4 mt-1 ml-2">
+    <a href="/signin?who=dis"><div class="px-3 py-2 bg-white text-black">Sign-in as DIS</div></a>
+    <a href="/signin?who=provider"><div class="px-3 py-2 bg-[#274C77] text-white">Sign-in as provider</div></a></div>
 </div>
 </div>
 <style>
