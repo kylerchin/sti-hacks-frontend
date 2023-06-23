62d12cbad7c00c08c1e2f4267926a8091b8c8e18
Audrey Nguyen <hoangsa9898@gmail.com>
2023-06-23 01:45:26 -0700

    half done w/ landing page

---
+++ src/routes/footer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	// TypeScript code for the Footer component
+</script>
+
+<footer>
+	<!-- Footer content -->
+	<p>Towards equitable healthcare.</p>
+</footer>
+
+<style>
+	/* Styles for the footer component */
+	footer {
+		background-color: #e7ecef;
+		color: #000000;
+		padding: 10px;
+		text-align: center;
+		font-family: 'IBM Plex Sans';
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+	}
+</style>
